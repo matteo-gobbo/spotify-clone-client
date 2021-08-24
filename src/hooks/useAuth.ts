c853c0c4a5e7a8cd5232eaf400b357fdfc31d88e
Matteo Gobbo <info@matteogobbo.it>
2021-08-25 00:54:22 +0200

    handle refresh token

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,16 +4,47 @@ import axios from "axios"
 const useAuth = (code: string) => {
   const [accessToken, setAccessToken] = useState()
   const [refreshToken, setRefreshToken] = useState()
-  const [expiresIn, setExpiresIn] = useState()
+  const [expiresIn, setExpiresIn] = useState(0)
 
   useEffect(() => {
-    axios.post('http://localhost:5000/login', {
-      code
-    }).then((res) => {
-      console.log(res.data)
-    })
+    axios
+      .post('http://localhost:5000/login', {
+        code
+      })
+      .then((res) => {
+        setAccessToken(res.data.accessToken)
+        setRefreshToken(res.data.refreshToken)
+        setExpiresIn(res.data.expiresIn)
+        window.history.pushState({}, '', '/')
+      })
+      .catch((e) => {
+        window.location.replace("http://localhost:3000");
+      })
 
   }, [code])
+
+  useEffect(() => {
+    if(!refreshToken || !expiresIn) return;
+
+    const interval = setInterval(() => {
+      axios
+        .post('http://localhost:5000/refresh', {
+          refreshToken
+        })
+        .then((res) => {
+          setAccessToken(res.data.accessToken)
+          setExpiresIn(res.data.expiresIn)
+        })
+        .catch((e) => {
+          window.location.replace("http://localhost:3000");
+        })
+    }, (expiresIn - 60) * 1000)
+
+    return () => clearInterval(interval)
+
+  }, [refreshToken, expiresIn])
+
+  return accessToken
 }
 
 export default useAuth
